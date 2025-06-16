edacef1c51ab035d417898df4f9bcfd8e4f9b3bb
Adhina Anup <148629512+adhinaanup@users.noreply.github.com>
2025-06-16 23:10:32 +0530

    Update insert delete.java

--- insert delete.java
+++ insert delete.java
@@ -1,1 +1,39 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read initial list size and elements
+        int n = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+
+        // Read number of queries
+        int q = scanner.nextInt();
+
+        // Process each query
+        for (int i = 0; i < q; i++) {
+            String action = scanner.next();  // "Insert" or "Delete"
+
+            if (action.equals("Insert")) {
+                int index = scanner.nextInt();
+                int value = scanner.nextInt();
+                list.add(index, value);
+            } else if (action.equals("Delete")) {
+                int index = scanner.nextInt();
+                list.remove(index);
+            }
+        }
+
+        // Print final list
+        for (int num : list) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+}
 
